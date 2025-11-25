c2d0a38f63c031fa1455aab0087d43295217c074
huebis <eliah.hueber@intergga.ch>
2025-11-25 15:40:48 +0100

    neu mit For-Schleife

--- hello.asm
+++ hello.asm
@@ -1,5 +1,6 @@
-extern printf
-global main
+extern printf ; nasm -f elf64 hello.asm -o hello.obj
+global main   ;   gcc -no-pie hello.obj -o hello
+
 
 
 section .data
@@ -11,11 +12,37 @@ section .text
 
 main:
     mov rdi, fmt
-    mov esi, 67
-    xor eax, eax     ; rax=0 wegen variadic function
+    mov r15D, 0 ;begin der for schlaufe
+    mov r14D, 1 ; kommt immer dazu
+    mov r13D, 100 ; bis ende ohne
+
+    vonneu:
+
+    cmp r15D, r13D
+    je fertig
+
+    xor eax, eax
+    mov rdi, fmt
+    mov esi, r15D
     call printf
 
+    add r15D, r14D
+
+
+
+    jmp vonneu
+
+
+
+
+
+
+
+
+    fertig:
 
     mov rax, 60         ; syscall exit begin vom Ende
     xor rdi, rdi
     syscall
+
+
